f1a9a321c89ae7534dcc52b0d98b1fe1d4efa853
LucasLinux <lucasmoraesbraga@gmail.com>
2026-04-08 22:43:02 -0300

    08/04/2026 pc

--- mobile/components/UserCard.js
+++ mobile/components/UserCard.js
@@ -1,8 +1,15 @@
 import { View, Text, TouchableOpacity} from "react-native"
+import * as React from 'react';
+import { List } from 'react-native-paper';
 
 export default function UserCard({user, onEdit, onDelete}){
     return (
         <View style={{padding: 15}}>
+            <List.Item
+                title={user.fullName}
+                description={user.role}
+                left={props => <List.Icon {...props} icon="folder" />}
+            />
             <Text>{user.fullName}</Text>
             <Text>{user.role}</Text>
             <TouchableOpacity onPress={onEdit}>
